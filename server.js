54e467760058be4d17f982478d1a964a2f95d93c
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-05-02 23:01:48 +0530

    database connection

---
+++ server.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+
+connectDB();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
